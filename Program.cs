1e6532161c9fad1fc88c8c1e5852133ab383a461
Kübranur Şahin <78857206+kubranursahin@users.noreply.github.com>
2022-05-13 12:44:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        //Aynen derste yaptığımız MyList gibi siz de kendi Dictonary sınıfınızı yazınız. (MyDictionary)
+
+        //Sadece ekleme(Add) metodunu yazınız
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Computer");
+            myDictionary.Add("Mouse");
+            myDictionary.Add("Switch");
+            myDictionary.Add("Laptop");
+            myDictionary.Add("Airpods");
+
+
+            Console.WriteLine(myDictionary.Count);
+
+        }
+    }
+}
+
